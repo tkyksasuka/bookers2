6ca356b4ef16d2be4ddfec2fdba7fd08eb72e89f
tkyksasuka <110615564+tkyksasuka@users.noreply.github.com>
2022-09-27 13:46:18 +0000

    [start]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
---
+++ db/migrate/20220927124257_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+
+      t.string :title
+      t.text :body
+      t.string :user_id
+
+      t.timestamps
+    end
+  end
+end
